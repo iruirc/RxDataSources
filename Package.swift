0e86dd7a8c9a1a7dd9499c304e6c0154a2f17df7
Dmitry Nuzhin <iruirc@gmail.com>
2023-12-07 17:48:38 +0300

    Update minimum supported OS versions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
   name: "RxDataSources",
   platforms: [
-    .iOS(.v9), .tvOS(.v9)
+   .iOS(.v12),
+   .tvOS(.v12)
   ],
   products: [
     .library(name: "RxDataSources", targets: ["RxDataSources"]),
